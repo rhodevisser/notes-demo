0d8ba97f25e5eb95e7c27bb119546dc67b610f7d
kapufskipauw <rosarhode@gmail.com>
2026-02-04 21:58:23 +0100

    fix: Temporary fix of $is
    Got warning Undefined array key "id" in, therefore I used AI to fix it temporary so I can continue for now.
    
    (need to solicit with senior why this was so in the first place)

--- index.php
+++ index.php
@@ -10,6 +10,13 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 
+$id = isset($_GET['id']) ? (int) $_GET['id'] : null;
+
+if (!$id) {
+	require 'controllers/index.php';
+	exit;
+}
+
 $query = "select * from posts where id = ? ";
 $post = $db->query($query, [$id])->fetch();
 
