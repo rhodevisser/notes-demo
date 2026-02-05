d6473850eebca201a14ace558d84eb75fb479718
kapufskipauw <rosarhode@gmail.com>
2026-02-05 20:20:40 +0100

    end: Ended code
    Because I don't need it for this exercise rn

--- index.php
+++ index.php
@@ -2,25 +2,11 @@
 
 require 'functions.php';
 require 'Database.php';
-// require 'router.php';
 
 
-$config = require('config.php');
-
-$db = new Database($config['database']);
-
-
-$id = isset($_GET['id']) ? (int) $_GET['id'] : null;
-
-if (!$id) {
-	require 'controllers/index.php';
-	exit;
-}
 
 $query = "select * from posts where id = ? ";
 $post = $db->query($query, [$id])->fetch();
-
-dumbDie($post);
 
 
 
