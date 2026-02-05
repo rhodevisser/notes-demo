298358186b43a4c6b0af559b527299cfb84e2e73
kapufskipauw <rosarhode@gmail.com>
2026-02-05 20:19:17 +0100

    create: Connected new mySQL DB
    I create a new root connection with TablePlus and editted the dbname to the correct one.

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ return [
     'database' => [
         'host' => 'localhost',
         'port'=> '3306',
-        'dbname' => 'mydemoapp',
+        'dbname' => 'mynotesdemo',
         'charset' => 'utf8mb4'
     ]
             
