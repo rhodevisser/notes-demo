9b5e8eb4cf5d19e10cce9af5c4cf6016f6eaa055
kapufskipauw <rosarhode@gmail.com>
2026-02-06 09:40:23 +0100

    edit: add dynamic links for notes
    Instead of hardcoding link within <a> as /note?id=1 - I used
    
    <? = $note['id'] ?>
    
    which TAKES the id from the note link itself.

--- controllers/notes.php
+++ controllers/notes.php
@@ -3,7 +3,7 @@
 $config = require('config.php');
 $db = new Database($config['database']); 
 
-$heading = 'Notes';
+$heading = 'Bryans Notes';
 
 $notes = $db->query('select * from notes where user_id = 1')->fetchAll();
 require "views/notes.view.php"; 
--- views/notes.view.php
+++ views/notes.view.php
@@ -5,8 +5,11 @@
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
     <?php foreach ($notes as $note) : ?>
-      <li>
-        <?= $note['body'] ?>
+      <li class="text-blue-500">
+        <a href="/note?id=<?= $note['id'] ?>"
+           class="text-blue-500 hover:underline">
+          <?= $note['body'] ?>
+        </a>
       </li>
 
 
