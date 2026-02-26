718190011ef36964a0a0c950430bcb66f2ad528e
kapufskipauw <rosarhode@gmail.com>
2026-02-26 12:20:40 +0100

    wip: 'Create note' page
    I added two lines that respond when the form is submitted and then graps data an attributes from the form.

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -2,4 +2,10 @@
 
 $heading = 'Create Note';
 
+// How to respond to the submission of the form:
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // How to grap data of attributes from the form:
+    dumbDie('$_POST');
+}
+
 require "views/note-create.view.php";
