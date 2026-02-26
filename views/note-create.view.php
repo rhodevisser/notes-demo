2f641917f532b40ad60e60418c3e3406693dd048
kapufskipauw <rosarhode@gmail.com>
2026-02-26 12:40:44 +0100

    add: Update note-create.view.php
    Added a tailwind form component

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -5,20 +5,20 @@
 <main>
   
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    
+
     <form method="POST">
-        <label for="body">
-            What do you want to tell? 
-        </label>
-        <div>
-            <textarea name="body" id="body"></textarea>
+        <div class="col-span-full">
+            <label for="body" class="block text-sm/6 font-medium text-white">
+                What is on your mind?
+            </label>
+            <div class="mt-2">
+                <textarea id="body" name="body" rows="3" class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"></textarea>
+            </div>
         </div>
-        
-        <p>
-            <button type="submit">
-                Create
-            </button>
-        </p>
+
+        <div class="mt-6 flex items-center justify-end gap-x-6">
+            <button type="submit" class="rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Save</button>
+        </div>
     </form>
 
   </div>
