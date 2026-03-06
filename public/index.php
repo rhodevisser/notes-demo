3fb6180e5a86d70a23d6be88c9f6ff83394914c5
kapufskipauw <rosarhode@gmail.com>
2026-03-06 12:21:14 +0100

    refactor: Create core folder
    Here I put all the generic class files

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ require BASE_PATH . 'functions.php';
 
 // this fn calls out DB
 spl_autoload_register(function ($class) {
-    require base_path($class . '.php');
+    require base_path("Core/" . $class . '.php');
 });
 
 require base_path('router.php');
