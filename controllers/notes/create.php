d02f40ad8f52d544efff00e4f98cd7f4d104f3dc
kapufskipauw <rosarhode@gmail.com>
2026-03-06 11:40:40 +0100

    Update create.php
    $errors var was declared only when  form was submitted, however $errors var needs to be there outside of that scope

--- controllers/notes/create.php
+++ controllers/notes/create.php
@@ -1,15 +1,17 @@
 <?php
 
-require 'Validator.php';
+require base_path('Validator.php');
 
 $config = require base_path('config.php');
 $db = new Database($config['database']);
+
+$errors = [];
 
 
 // How to respond to the submission of the form:
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    $errors = [];
+
 
     if (! Validator::string($_POST['body'], 1, 200)) {
         $errors['body'] = 'A body of no more than 200 characters is required';
