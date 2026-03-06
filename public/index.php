be5f4f1baaf061459b8602438e7574492bde476c
kapufskipauw <rosarhode@gmail.com>
2026-03-06 12:11:44 +0100

    refactor: Added spl_autoload_register fn
    This fn can replace all the separated 'require' because it will call a class that has not been loaded yet. This will be triggered by new Database..

--- public/index.php
+++ public/index.php
@@ -4,6 +4,9 @@ const BASE_PATH = __DIR__ . '/../';
 
 require BASE_PATH . 'functions.php';
 
-require base_path('Database.php');
-require base_path('Response.php');
+// this fn calls out DB
+spl_autoload_register(function ($class) {
+    require base_path($class . '.php');
+});
+
 require base_path('router.php');
