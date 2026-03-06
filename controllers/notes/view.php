fd5d6c33657e749e225e2552e366b126d4f079b5
kapufskipauw <rosarhode@gmail.com>
2026-03-06 16:12:52 +0100

    feat: Added query to delete button
    I added an if statement that checks if the request method is "post"
    
    IF so it: selects the id of the note and authorizes if the id is paired with the active user_id > if so it deletes that note.

--- controllers/notes/view.php
+++ controllers/notes/view.php
@@ -7,13 +7,29 @@ $db = new Database($config['database']);
 
 $currentUserId = 1;
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    // form was submitted delete the current note
+    $db->query('delete from notes where id = :id', [
+        'id' => $_GET['id']
+    ]);
+
+    header('location: /notes');
+    die();
+} else {
+}
+
 $note = $db->query('select * from notes where id = :id', [
     'id' => $_GET['id']
 ])->findOrFail();
 
 authorize($note['user_id'] === $currentUserId);
-
-
 
 view("notes/view.view.php", [
     'heading' => 'Note',
